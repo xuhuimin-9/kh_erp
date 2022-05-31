d4c40f34db29edb60df7fe2937121b22298f081e
xuhuimin-9 <2469685617@qq.com>
2022-05-31 11:43:53 +0800

    出库日志文件中增加申领仓库的字段
    Signed-off-by: xuhuimin-9 <2469685617@qq.com>

--- storage.py
+++ storage.py
@@ -258,7 +258,8 @@ def export_in_storage_log(data):
         value = float(Decimal(str(logInfo[i - 1]['price'] * int(logInfo[i - 1]['count']))))
         sheet.write(i, 7, value)
         sheet.write(i, 8, logInfo[i - 1]['invoice_type'])
-        sheet.write(i, 9, logInfo[i - 1]['tax_rate'])
+        value = "%.2f%%" % (logInfo[i - 1]['tax_rate']*100)
+        sheet.write(i, 9, value)
         value = float(Decimal(str(logInfo[i - 1]['tax_price']/float(logInfo[i - 1]['count']))))
         sheet.write(i, 10, value)
         sheet.write(i, 11, logInfo[i - 1]['tax_price'])
@@ -299,35 +300,38 @@ def export_out_storage_log(data):
     work_book = xlwt.Workbook(encoding='utf-8')
     sheet = work_book.add_sheet('sheet')
     sheet.write(0, 0, '编号')
-    sheet.write(0, 1, '项目名称')
-    sheet.write(0, 2, '仓库名称')
-    sheet.write(0, 3, '类别名称')
-    sheet.write(0, 4, '商品名称')
-    sheet.write(0, 5, '商品单位')
-    sheet.write(0, 6, '出库数量')
-    sheet.write(0, 7, '出库单价')
-    sheet.write(0, 8, '发票类型')
-    sheet.write(0, 9, '税率')
-    sheet.write(0, 10, '出库总价')
-    sheet.write(0, 11, '含税总价')
-    sheet.write(0, 12, '出库时间')
+    sheet.write(0, 1, '申领项目')
+    sheet.write(0, 2, '申领仓库')
+    sheet.write(0, 3, '实际仓库')
+    sheet.write(0, 4, '类别名称')
+    sheet.write(0, 5, '商品名称')
+    sheet.write(0, 6, '商品单位')
+    sheet.write(0, 7, '出库数量')
+    sheet.write(0, 8, '出库单价')
+    sheet.write(0, 9, '发票类型')
+    sheet.write(0, 10, '税率')
+    sheet.write(0, 11, '出库总价')
+    sheet.write(0, 12, '含税总价')
+    sheet.write(0, 13, '出库时间')
     logInfo=list(result)
     number=len(logInfo)
     for i in range(1,number+1):
         sheet.write(i, 0, logInfo[i - 1]['id'])
         sheet.write(i, 1, logInfo[i - 1]['project'])
-        sheet.write(i, 2, logInfo[i - 1]['storage_name'])
-        sheet.write(i, 3, logInfo[i - 1]['category_name'])
-        sheet.write(i, 4, logInfo[i - 1]['name'])
-        sheet.write(i, 5, logInfo[i - 1]['unit'])
-        sheet.write(i, 6, logInfo[i - 1]['count'])
-        sheet.write(i, 7, logInfo[i - 1]['price'])
-        sheet.write(i, 8, logInfo[i - 1]['invoice_type'])
-        sheet.write(i, 9, logInfo[i - 1]['tax_rate'])
-        sheet.write(i, 10, logInfo[i - 1]['total_price'])
-        sheet.write(i, 11, logInfo[i - 1]['tax_price'])
+        sheet.write(i, 2, logInfo[i - 1]['apply_storage'])
+        sheet.write(i, 3, logInfo[i - 1]['storage_name'])
+        sheet.write(i, 4, logInfo[i - 1]['category_name'])
+        sheet.write(i, 5, logInfo[i - 1]['name'])
+        sheet.write(i, 6, logInfo[i - 1]['unit'])
+        sheet.write(i, 7, logInfo[i - 1]['count'])
+        sheet.write(i, 8, logInfo[i - 1]['price'])
+        sheet.write(i, 9, logInfo[i - 1]['invoice_type'])
+        value = "%.0f%%" % (logInfo[i - 1]['tax_rate'] * 100)
+        sheet.write(i, 10, value)
+        sheet.write(i, 11, logInfo[i - 1]['total_price'])
+        sheet.write(i, 12, logInfo[i - 1]['tax_price'])
         value=logInfo[i - 1]['create_time'].strftime("%Y/%m/%d")
-        sheet.write(i, 12, value)
+        sheet.write(i, 13, value)
 
     today=datetime.today()
     today=today.strftime("%Y/%m/%d")
@@ -360,7 +364,7 @@ def filter_in_storage_log(data):
     return result
 
 
-# 筛选指定时间段的入库日志内容
+# 筛选指定时间段的出库日志内容
 def filter_out_storage_log(data):
     storage = data['storage']
     timeSlot = data['datetime']
@@ -444,8 +448,8 @@ def material_returned_info(data):
     price = borrow_info['price']
     invoice = borrow_info['invoice_type']
     tax = borrow_info['tax_rate']
-    total_price = borrow_info['total_price']
-    tax_price = borrow_info['tax_price']
+    total_price = -borrow_info['total_price']
+    tax_price = -borrow_info['tax_price']
     project=borrow_info['project']
     supplier=borrow_info['supplier']
     storage = borrow_info['storage_name']
